9f47d873e9f6e95798ffa97773200aa29c518ee2
David VANMAK <david.vanmak.lwg@gmail.com>
2023-06-08 12:53:36 +0200

    [ADD] PostMessageUsecase

---
+++ crafty/src/tests/post-message.usecase.ts
@@ -0,0 +1,51 @@
+export type postMessageCommand = {
+    id: string,
+    text: string,
+    author: string
+}
+export type Message = {
+    id: string,
+    text: string,
+    author: string,
+    publishedAt: Date
+}
+
+export interface MessageRepository {
+    save(message: Message): void
+}
+
+export interface DateProvider {
+    getNow(): Date
+}
+
+export class MessageTooLongError extends Error {
+}
+
+export class EmptyMessageError extends Error {
+}
+export class WhiteSpacesMessageError extends Error {
+}
+export class PostMessageUseCase {
+    constructor(
+        private readonly messageRepository: MessageRepository,
+        private readonly dateProvider: DateProvider
+    ) { }
+    handle(postMessageCommand: postMessageCommand) {
+        if (postMessageCommand.text.length >= 280)
+            throw new MessageTooLongError()
+
+        if (postMessageCommand.text.length == 0)
+            throw new EmptyMessageError()
+
+        if (postMessageCommand.text
+            .trim().length == 0)
+            throw new WhiteSpacesMessageError()
+
+        this.messageRepository.save({
+            id: postMessageCommand.id,
+            text: postMessageCommand.text,
+            author: postMessageCommand.author,
+            publishedAt: this.dateProvider.getNow()
+        })
+    }
+}
--- crafty/src/tests/posting-message.spec.ts
+++ crafty/src/tests/posting-message.spec.ts
@@ -1,59 +1,115 @@
+import { DateProvider, Message, MessageRepository, MessageTooLongError, PostMessageUseCase, WhiteSpacesMessageError, postMessageCommand } from "./post-message.usecase"
+
 describe("Feature: posting a message", () => {
+    let fixtures: Fixtures
+    beforeEach(() => {
+        fixtures = createFixture()
+    })
     describe("Rule: A message can contain a maximum of 280 characteres", () => {
         test("Alice can post a message on her timelune", () => {
-            givenNowis(new Date('2023-01-19T19:00:00.000Z'))
+            fixtures.givenNowis(new Date('2023-01-19T19:00:00.000Z'))
 
-            whenUserPostsAmessage({
+            fixtures.whenUserPostsAmessage({
                 id: 'message-id',
                 author: 'Alice',
                 text: 'Hellow world',
             })
 
-            thenPostMessageshouldBe({
+            fixtures.thenPostMessageshouldBe({
                 id: 'message-id',
                 text: 'Hellow world',
                 author: 'Alice',
                 publishedAt: new Date('2023-01-19T19:00:00.000Z')
             })
         })
+
+        test("Alice can not post a message with more than 280 characters", () => {
+            const textWithMoreThan280Characteres: string =
+                "In ex Lorem consequat veniam labore mollit quis aliqua quis eu minim. Commodo adipisicing dolore tempor culpa consectetur ea magna ad sit exercitation culpa adipisicing. Labore non enim laboris dolore commodo officia sint dolor enim. Consectetur eu in ullamco non excepteur enim te."
+
+            fixtures.givenNowis(new Date('2023-01-19T19:00:00.000Z'))
+
+            fixtures.whenUserPostsAmessage({
+                id: 'message-id',
+                author: 'Alice',
+                text: textWithMoreThan280Characteres,
+            })
+
+            fixtures.thenErrorShouldBe(MessageTooLongError)
+        })
+        test("Alice can not post a message with only white spaces", () => {
+            const textWithWhiteSpaces: string =
+                "      "
+
+            fixtures.givenNowis(new Date('2023-01-19T19:00:00.000Z'))
+
+            fixtures.whenUserPostsAmessage({
+                id: 'message-id',
+                author: 'Alice',
+                text: textWithWhiteSpaces,
+            })
+
+            fixtures.thenErrorShouldBe(WhiteSpacesMessageError)
+        })
     })
 })
 
-let message: {
-    id: string,
-    text: string,
-    author: string,
-    publishedAt: Date
-} = {
-    id: 'message-id',
-    text: 'Hellow world',
-    author: 'Alice',
-    publishedAt: new Date('2023-01-19T19:00:00.000Z')
-}
-let now: Date
-function givenNowis(actual_date: Date) {
-    now = actual_date
-}
 
 
-function whenUserPostsAmessage(postMessageCommand: {
-    id: string,
-    text: string,
-    author: string
-}) {
-    message = {
-        id: postMessageCommand.id,
-        text: postMessageCommand.text,
-        author: postMessageCommand.author,
-        publishedAt: now
+
+
+
+
+class InMemoryMessageRepository implements MessageRepository {
+    public message!: Message
+
+    save(msg: Message): void {
+        this.message = msg;
+    }
+}
+class StubeDateProvider implements DateProvider {
+    now!: Date
+    getNow(): Date {
+        return this.now
     }
 }
 
-function thenPostMessageshouldBe(expectedMessage: {
-    id: string,
-    text: string,
-    author: string,
-    publishedAt: Date
-}) {
-    expect(expectedMessage).toEqual(message)
+
+
+
+const createFixture = () => {
+    const dateProvider = new StubeDateProvider()
+
+    const messageRepository: MessageRepository = new InMemoryMessageRepository()
+
+    const postMessageUseCase: PostMessageUseCase = new PostMessageUseCase(
+        messageRepository,
+        dateProvider
+    )
+
+
+
+    let thrownError: Error
+    return {
+        givenNowis(now: Date) {
+            dateProvider.now = now
+        },
+        whenUserPostsAmessage(postMessageCommand: postMessageCommand) {
+            try {
+                postMessageUseCase.handle(postMessageCommand)
+            } catch (error: any) {
+                thrownError = error
+            }
+        },
+        thenPostMessageshouldBe(expectedMessage: Message) {
+            expect(expectedMessage).toEqual((messageRepository as InMemoryMessageRepository).message);
+        },
+        thenErrorShouldBe(expectErrorClass: new () => Error) {
+            expect(thrownError).toBeInstanceOf(expectErrorClass)
+        }
+
+
+    }
 }
+
+type Fixtures = ReturnType<typeof createFixture>
